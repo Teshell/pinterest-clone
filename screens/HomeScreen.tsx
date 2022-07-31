1e91a7d75a20332aade067c465e7cc270745d6d5
Teshell <fayrongeek@gmail.com>
2022-07-31 13:10:38 +0100

    first commit

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,40 @@
+import { ScrollView, StyleSheet } from "react-native";
+import Pin from "../components/Pin";
+import { View } from "../components/Themed";
+
+import { RootTabScreenProps } from "../types";
+
+export default function HomeScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Pin
+          pin={{
+            title: "Title",
+            image:
+              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/2.jpeg",
+          }}
+        />
+
+        <Pin
+          pin={{
+            title: "2nd Title",
+            image:
+              "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/3.jpeg",
+          }}
+        />
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+});
