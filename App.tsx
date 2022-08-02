dc5ef7e7371c36ac94a7751c0e55a55f7a316d26
Teshell <fayrongeek@gmail.com>
2022-08-02 12:18:37 +0100

    initialize nhost backend

--- App.tsx
+++ App.tsx
@@ -5,6 +5,15 @@ import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
+import { NhostClient, NhostReactProvider } from "@nhost/react";
+import * as SecureStore from "expo-secure-store";
+
+const nhost = new NhostClient({
+  backendUrl: "https://oeqpfaruyvebytngyvrt.nhost.run",
+  clientStorageType: "expo-secure-storage",
+  clientStorage: SecureStore,
+});
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -13,10 +22,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NhostReactProvider nhost={nhost}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </NhostReactProvider>
     );
   }
 }
